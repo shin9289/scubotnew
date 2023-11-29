261aa1903b64868ff69f320532a87d7ba73d4600
unknown <gindy920809@gmail.com>
2023-11-29 11:25:08 +0800

    test

--- scubotgo/module/func.py
+++ scubotgo/module/func.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+import requests
+from bs4 import BeautifulSoup
 
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
@@ -10,3 +12,17 @@ def sendText(event):
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='成功'))
     except:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='錯誤'))
+
+def get_latest_news():
+    url = "https://www-ch.scu.edu.tw/october/search?category=5"
+    res = requests.get(url, verify=False)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    titles = soup.select('.table.table-striped [headers="header"] a')
+
+    news_info = []
+    for each_title in titles[:5]:
+        title_text = each_title.text.strip()
+        link_href = each_title['href'].strip()
+        news_info.append(f"{title_text}\n{link_href}\n")
+
+    return "\n".join(news_info) 
